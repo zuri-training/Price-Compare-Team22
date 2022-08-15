39d38943342268a5b131b185d1649ebe1f2e920e
nurudeen2432 <85231020+nurudeen2432@users.noreply.github.com>
2022-08-15 14:57:21 +0100

    'commit'

--- pricezilla/pricezilla/settings.py
+++ pricezilla/pricezilla/settings.py
@@ -74,13 +74,14 @@ SITE_ID = 1
 # }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+   
 ]
 
 ROOT_URLCONF = 'pricezilla.urls'
